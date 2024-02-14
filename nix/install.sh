5374f1a57b6f45cec2d4f41f4aa8a05913c5171c
Jon Lauridsen <mail@jonlauridsen.com>
2024-02-14 16:57:12 +0100

    nix/More recordings

--- nix/install.sh
+++ nix/install.sh
@@ -1,10 +1,22 @@
 #!/bin/zsh
 set +euo pipefail
 
-command -v nix-build &> /dev/null || { echo "Installing Nix..."; sh <(curl -L https://nixos.org/nix/install); }
-command -v direnv &> /dev/null || { echo "Installing direnv..."; nix-env -iA nixpkgs.direnv; }
-type direnv &>/dev/null || {
-    echo 'Direnv is not active. Activate it like this: eval "$(direnv hook zsh)" >> ~/.zshrc; source ~/.zshrc';
-    exit 1;
+command -v nix-build &> /dev/null || {
+  echo "Installing Nix..."; sh <(curl -L https://nixos.org/nix/install);
+  echo "Nix installed, please restart terminal & re-run this script"
+  exit 1
 }
-echo "Setup complete."
+
+mkdir -p ~/.config/nix && echo "experimental-features = nix-command flakes" > ~/.config/nix/nix.conf
+
+command -v direnv &> /dev/null || {
+  echo "Installing direnv..."; nix-env -iA nixpkgs.direnv;
+}
+
+grep -q 'eval "\$(direnv hook zsh)"' ~/.zshrc || {
+  echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+  echo "Hook added to .zshrc, please restart terminal & re-run this script"
+  exit 1;
+}
+
+echo "Setup complete"
