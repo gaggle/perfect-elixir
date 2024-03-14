b3ab242395d4e2c84ca8f87ad0f7058ceed8d811
Jon Lauridsen <mail@jonlauridsen.com>
2024-03-14 20:30:08 +0100

    cowboy_example/Add Cowboy application and server setup
    Added Cowboy to the list of extra applications and created a new module for application setup in mix.exs. The server setup includes defining a route and initializing a HTTP listener on port 8080. It also added a HelloHandler to reply with a "Hello World!" message.

--- cowboy_example/lib/cowboy_example.ex
+++ cowboy_example/lib/cowboy_example.ex
@@ -1,18 +1,41 @@
 defmodule CowboyExample do
-  @moduledoc """
-  Documentation for `CowboyExample`.
-  """
+  def start_server do
+    # Set up the routing table for the Cowboy server, so root req
+    #uests ("/") direct to our handler.
+    dispatch = :cowboy_router.compile([{:_, [
+      {"/", CowboyExample.HelloWorldHandler, []}
+    ]}])
 
-  @doc """
-  Hello world.
+    # Start the Cowboy server in "clear mode" aka plain HTTP
+    #   options - Configuration options for the server itself
+    #             (this also supports which IP to bind to,
+    #             SSL details, etc.)
+    #   `env`   - Configuration map for how the server
+    #             handles HTTP requests
+    #             (this also allows configuring timeouts,
+    #             compression settings, etc.)
+    {:ok, _} =
+      :cowboy.start_clear(
+        :my_name,
+        [{:port, 8080}],
+        %{env: %{dispatch: dispatch}}
+      )
 
-  ## Examples
+    IO.puts("Cowboy server started on port 8080")
+  end
+end
 
-      iex> CowboyExample.hello()
-      :world
+defmodule CowboyExample.HelloWorldHandler do
+  # `init/2` is the entry point for handling a new HTTP request
+  # in Cowboy
+  def init(req, _opts) do
+    req = :cowboy_req.reply(200, %{
+      "content-type" => "text/html"
+    }, "<h1>Hello World!</h1>", req)
 
-  """
-  def hello do
-    :world
+    # Return `{:ok, req, state}` where `state` is
+    # handler-specific state data; here, it's `:nostate`
+    # as we do not maintain any state between requests.
+    {:ok, req, :nostate}
   end
 end
