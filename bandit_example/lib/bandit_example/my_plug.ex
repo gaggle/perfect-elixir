cde9a0174198e7059fd17125b3d23081695e3d65
Jon Lauridsen <mail@jonlauridsen.com>
2024-03-17 21:18:10 +0100

    bandit_example/Make a database query in BanditExample.MyPlug

--- bandit_example/lib/bandit_example/my_plug.ex
+++ bandit_example/lib/bandit_example/my_plug.ex
@@ -4,8 +4,11 @@ defmodule BanditExample.MyPlug do
   def init(options), do: options
 
   def call(conn, _options) do
+    {:ok, %Postgrex.Result{rows: [[current_time]]}} =
+      Postgrex.query(:bandit_db, "SELECT NOW() as current_time", [])
+
     conn
     |> put_resp_content_type("text/plain")
-    |> send_resp(200, "Hello, World!")
+    |> send_resp(200, "Hello, World! It's #{current_time}")
   end
 end
