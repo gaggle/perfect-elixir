8f2d737106fa85cd35d2e917e571e14c4576d595
Jon Lauridsen <jon.lauridsen@maersk.com>
2024-12-15 16:18:54 +0700

    Add process tree dependency

--- mix.exs
+++ mix.exs
@@ -57,7 +57,8 @@ defmodule MyApp.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.2"}
+      {:bandit, "~> 1.2"},
+      {:process_tree, "~> 0.1.2"},
     ]
   end
 
