ea39f61b00e37b0183f794c2cac73d3024bd57fb
Jon Lauridsen <mail@jonlauridsen.com>
2025-03-06 22:26:11 +0100

    Change `Inventory` to callbacks, and add `RealInventory` & `FakeInventory` implementations

--- lib/inventory.ex
+++ lib/inventory.ex
@@ -1,3 +1,18 @@
 defmodule Inventory do
-  def all_products(), do: ["a"]
+  @callback all_products() :: [String.t()]
+end
+
+defmodule RealInventory do
+  @behaviour Inventory
+  @impl Inventory
+  def all_products() do
+    # pretend this does sophisticated querying and data-aggregations
+    ["a"]
+  end
+end
+
+defmodule FakeInventory do
+  @behaviour Inventory
+  @impl Inventory
+  def all_products(), do: []
 end
