d01018a4facb748a8486d5699ad7034102b43941
Jon Lauridsen <mail@jonlauridsen.com>
2025-03-06 22:30:26 +0100

    Inject `FakeInventory` in `WarehouseTest`

--- test/warehouse_test.exs
+++ test/warehouse_test.exs
@@ -2,13 +2,7 @@ defmodule WarehouseTest do
   use ExUnit.Case, async: true
   import InjectorTree, only: [inject: 2]
 
-  setup do
-    defmodule InventoryStub do
-      def all_products(), do: []
-    end
-
-    inject(Inventory, InventoryStub)
-  end
+  setup do: inject(RealInventory, FakeInventory)
 
   test "warehouse is empty when inventory has no products" do
     assert Warehouse.empty?() == true
