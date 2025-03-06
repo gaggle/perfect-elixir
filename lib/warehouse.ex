9d14279ed56d6a39e34b00d0fac61a1b7abdd4aa
Jon Lauridsen <mail@jonlauridsen.com>
2025-03-06 21:39:54 +0100

    Use argument injection for Warehouse & test

--- lib/warehouse.ex
+++ lib/warehouse.ex
@@ -1,4 +1,3 @@
 defmodule Warehouse do
-  def empty?(), do: inventory_impl().all_products() == []
-  defp inventory_impl(), do: Application.get_env(:myapp, :inventory, Inventory)
+  def empty?(inventory), do: inventory.all_products() == []
 end
--- test/warehouse_test.exs
+++ test/warehouse_test.exs
@@ -1,15 +1,11 @@
 defmodule WarehouseTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
 
-  setup do
+  test "warehouse is empty when inventory has no products" do
     defmodule InventoryStub do
       def all_products(), do: []
     end
 
-    Application.put_env(:myapp, :inventory, InventoryStub)
-  end
-
-  test "warehouse is empty when inventory has no products" do
-    assert Warehouse.empty?() == true
+    assert Warehouse.empty?(InventoryStub) == true
   end
 end
